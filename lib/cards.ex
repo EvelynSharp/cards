a839429d39beb70e8871e07c5355f3a13abe5351
Evelyn Sharp <yihongshi90@gmail.com>
2017-11-27 21:58:19 -0800

    create deck

--- lib/cards.ex
+++ lib/cards.ex
@@ -2,7 +2,13 @@
 defmodule Cards do
 
   def create_deck do
-    ["Ace", "Two", "Three"]
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
+  #or use List.flattern() after nested for loops 
   end
 
   def shuffle(deck) do
